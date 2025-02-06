ae1a143777f094dbefb6143ff4ead99fcfeb5ec1
unknown <ajaysaju987@gmail.com>
2025-02-07 03:49:44 +0530

    TopK

--- Neet_Code_150/Top_K_Frequent_Elements.java
+++ Neet_Code_150/Top_K_Frequent_Elements.java
@@ -1,5 +1,11 @@
 package Neet_Code_150;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 // Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 // Example 1:
@@ -11,7 +17,40 @@ package Neet_Code_150;
 // Input: nums = [1], k = 1
 // Output: [1]
 
-
 public class Top_K_Frequent_Elements {
-    
+    public static void main(String[] args) {
+        int[] a = { 1, 1, 1, 2, 2, 3,5,5,5,5,5 };
+        int[] ans = topKFrequent(a, 2);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[] topKFrequent(int[] nums, int k) {
+
+        if (nums.length == k) {
+            return nums;
+        }
+
+        HashMap<Integer, Integer> myMap = new HashMap<>();
+        for (int key : nums) {
+
+            myMap.put(key, myMap.getOrDefault(key, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(
+                Comparator.comparingInt(Map.Entry::getValue));
+
+        for (Map.Entry<Integer, Integer> value : myMap.entrySet()) {
+
+            minHeap.offer(value);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        int[] topKElements = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            topKElements[i] = minHeap.poll().getKey();
+        }
+        return topKElements;
+    }
+
 }
